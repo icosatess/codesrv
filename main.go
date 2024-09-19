745f431af3e7e875ef04416eb89a6d46fc747e6e
Icosatess <icosatess@example.com>
2024-09-18 23:51:34 -0400

    Force plain text for codesrv

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 )
 
 func root(w http.ResponseWriter, r *http.Request) {
@@ -18,12 +22,27 @@ func root(w http.ResponseWriter, r *http.Request) {
 	`))
 }
 
+func codesrv(w http.ResponseWriter, r *http.Request) {
+	cleanPath := path.Clean(r.URL.Path)
+	dir, file := path.Split(cleanPath)
+	_ = dir
+	fullpath := filepath.Join(`C:\Users\Icosatess\Source\codesrv`, file)
+	f, ferr := os.Open(fullpath)
+	if ferr != nil {
+		panic(ferr)
+	}
+	defer f.Close()
+
+	w.Header().Set("Content-Type", "text/plain")
+	io.Copy(w, f)
+}
+
 func main() {
 	http.HandleFunc("/", root)
 	http.Handle("/minimapui/", http.FileServer(http.Dir(`C:\Users\Icosatess\Source`)))
 	http.Handle("/minimapsrv/", http.FileServer(http.Dir(`C:\Users\Icosatess\Source`)))
 	http.Handle("/minimapext/", http.FileServer(http.Dir(`C:\Users\Icosatess\Source`)))
-	http.Handle("/codesrv/", http.FileServer(http.Dir(`C:\Users\Icosatess\Source`)))
+	http.HandleFunc("/codesrv/", codesrv)
 
 	// TODO: serve as plain text
 	// TODO: add a disallow-list for dotfiles and other stuff viewers shouldn't see

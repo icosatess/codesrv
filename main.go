89177383b2464d4c66cc6aea919dc68c3d6f1a55
Icosatess <icosatess@example.com>
2024-09-19 00:39:24 -0400

    Directory listing works

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -31,7 +32,9 @@ func (h workspaceFolderHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 	var pathComponents []string
 
 	dir, file := path.Split(cleanPath)
-	pathComponents = append(pathComponents, file)
+	if file != "" {
+		pathComponents = append(pathComponents, file)
+	}
 	for dir != "/"+string(h)+"/" {
 		dir, file = path.Split(dir[:len(dir)-1])
 		pathComponents = append(pathComponents, file)
@@ -40,15 +43,40 @@ func (h workspaceFolderHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 	slices.Reverse(pathComponents)
 	fullpath := filepath.Join(pathComponents...)
 
-	f, ferr := os.Open(fullpath)
-	if ferr != nil {
-		// TODO: handle file not found
-		panic(ferr)
+	fi, fierr := os.Stat(fullpath)
+	if fierr != nil {
+		panic(fierr)
 	}
-	defer f.Close()
 
-	w.Header().Set("Content-Type", "text/plain")
-	io.Copy(w, f)
+	if fi.IsDir() {
+		des, deserr := os.ReadDir(fullpath)
+		if deserr != nil {
+			panic(deserr)
+		}
+		w.Write([]byte(`
+		<!DOCTYPE html>
+		<title>Code server</title>
+		<ul>
+		`))
+		for _, de := range des {
+			currentPath := r.URL.Path
+			deName := de.Name()
+			deFullPath := path.Join(currentPath, deName)
+			// TODO: XSS can happen here a lot
+			w.Write([]byte(fmt.Sprintf(`<li><a href="%s">%s</a>`, deFullPath, deName)))
+		}
+		w.Write([]byte("</ul>"))
+	} else {
+		f, ferr := os.Open(fullpath)
+		if ferr != nil {
+			// TODO: handle file not found
+			panic(ferr)
+		}
+		defer f.Close()
+
+		w.Header().Set("Content-Type", "text/plain")
+		io.Copy(w, f)
+	}
 }
 
 func main() {

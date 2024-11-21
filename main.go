ec41cc5efef446adc2cb986df2db89d1ac38d3a5
Icosatess <icosatess@example.com>
2024-11-21 00:22:31 -0500

    Dangerous prototype for sidebar

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"io"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
@@ -94,6 +96,7 @@ func serveWorkspaceFolder(w http.ResponseWriter, r *http.Request) {
 
 	var pathComponents []string
 
+	// TODO: replace all of this with filepath.FromSlash
 	dir, file := path.Split(cleanPath)
 	if file == "secrets.json" {
 		http.Error(w, "Icosatess has disallowed public viewing of this file", http.StatusForbidden)
@@ -200,7 +203,7 @@ const frameTemplate = `
 
 <frameset cols="20%, 80%">
 <frame src="/sidebar">
-<frame src="{{.ContentPath}}">
+<frame src="{{.ContentPath}}" name="contentpane">
 </frameset>
 <noframes>
 no frames content here
@@ -225,8 +228,73 @@ func frameTest(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+type parentDirectory struct {
+	RelativePath string
+	Path         string
+	Items        []*parentDirectory
+}
+
 func sidebar(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("sidebar goes here"))
+	var buf bytes.Buffer
+	buf.WriteString(`<!DOCTYPE html>
+<meta charset="UTF-8">
+<title>Sidebar</title>
+<style>
+:root {
+	color-scheme: light dark;
+}
+</style>
+<ul>
+`)
+	basePath := `C:\Users\Icosatess\Source\codesrv`
+	var parents []*parentDirectory = []*parentDirectory{{
+		Path: basePath,
+	}}
+	filepath.WalkDir(basePath, func(path string, d fs.DirEntry, err error) error {
+		log.Printf("walkdir callback called with path=%s, d=%+v, err=%+v", path, d, err)
+		log.Printf("current state of parents is size %d: %+v", len(parents), parents)
+		if err != nil {
+			log.Printf("error walking dir %s in %s: %v", d, path, err)
+			panic(err)
+		}
+
+		dir := filepath.Dir(path)
+		for i := len(parents) - 1; i >= 0; i-- {
+			if parents[i].Path == dir {
+				parents = parents[:i+1]
+				after, ok := strings.CutPrefix(path, basePath)
+				if !ok {
+					panic("couldn't cut the base path out")
+				}
+				pd := parentDirectory{Path: path, RelativePath: after}
+				parents[i].Items = append(parents[i].Items, &pd)
+				if d.IsDir() {
+					parents = append(parents, &pd)
+				}
+				break
+			}
+		}
+		return nil
+	})
+	// buf.WriteString(fmt.Sprintf("<h1>%s</h1>", path))
+	DoRecursionInto(&buf, "/codesrv", *parents[0])
+	buf.WriteString("</ul>")
+	w.Write(buf.Bytes())
+}
+
+func DoRecursionInto(buf *bytes.Buffer, prefix string, pd parentDirectory) {
+	log.Printf("DoRecursionInto called for %s", pd.Path)
+	log.Printf("It has items %+v", pd.Items)
+	urlPath := path.Join(prefix, filepath.ToSlash(pd.RelativePath))
+	buf.WriteString(fmt.Sprintf(`<li><a href="%s" target="contentpane">%s</a>`, urlPath, filepath.Base(pd.Path)))
+	if len(pd.Items) == 0 {
+		return
+	}
+	buf.WriteString("<ul>")
+	for _, item := range pd.Items {
+		DoRecursionInto(buf, prefix, *item)
+	}
+	buf.WriteString("</ul>")
 }
 
 func main() {
